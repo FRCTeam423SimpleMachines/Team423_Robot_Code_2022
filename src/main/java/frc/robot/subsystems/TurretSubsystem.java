fa3681880367902cfbebcdc825d2534f5b809da1
GL-321 <99373370+GL-321@users.noreply.github.com>
2022-02-26 11:05:35 -0500

    Output Encoder Value to dashboard

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.TurretConstants;
 
@@ -11,14 +12,24 @@ public class TurretSubsystem extends SubsystemBase {
   
   private final CANSparkMax turretMotor = new CANSparkMax(TurretConstants.kTurretMotorPort, MotorType.kBrushless);
 
-  private RelativeEncoder m_encoderT = turretMotor.getEncoder();
+  private RelativeEncoder TurretEncoder = turretMotor.getEncoder();
 
   /** Creates a new ExampleSubsystem. */
   public TurretSubsystem() {}
 
+  public double getEncoderAngle(RelativeEncoder encoder) {
+    double encoderValue = encoder.getPosition();
+    return encoderValue*8.57142857143;
+  }
+
+  public void logToDashboard() {
+    SmartDashboard.putNumber("Turret/Turret Angle", getEncoderAngle(TurretEncoder));
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    logToDashboard();
   }
 
   @Override
