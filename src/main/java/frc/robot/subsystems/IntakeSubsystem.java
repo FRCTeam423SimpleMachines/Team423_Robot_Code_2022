b45c7774d6cdae5ef5f9927ea2543e273a55469b
GL-321 <99373370+GL-321@users.noreply.github.com>
2022-02-26 10:50:28 -0500

    Output Encoder Data to dashboard

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
@@ -11,15 +13,21 @@ public class IntakeSubsystem extends SubsystemBase {
   private final CANSparkMax intakeMotor = new CANSparkMax(IntakeConstants.kIntakeMotorPort, MotorType.kBrushless);
   private final CANSparkMax intakeArmMotor = new CANSparkMax(IntakeConstants.kIntakeArmMotorPort, MotorType.kBrushless);
 
-  private RelativeEncoder m_IntakeEncoder = intakeMotor.getEncoder();
-  private RelativeEncoder m_IntakeArmEncoder = intakeArmMotor.getEncoder();
+  private RelativeEncoder IntakeEncoder = intakeMotor.getEncoder();
+  private RelativeEncoder IntakeArmEncoder = intakeArmMotor.getEncoder();
 
   /** Creates a new ExampleSubsystem. */
   public IntakeSubsystem() {}
 
+  public void logToDashboard() {
+    SmartDashboard.putNumber("Intake/Intake Speed", IntakeEncoder.getVelocity());
+    SmartDashboard.putNumber("Intake/Intake Arm Position", IntakeEncoder.getPosition());
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    logToDashboard();
   }
 
   @Override
